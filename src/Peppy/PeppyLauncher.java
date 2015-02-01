197f31e74e01c073f8cb3c173f1a555f933b77e4
Brian Risk <brian@geneffects.com>
2015-02-01 11:41:32 -0500

    Added launcher class so that maximum memory can be set when jar is launched

---
+++ src/Peppy/PeppyLauncher.java
@@ -0,0 +1,21 @@
+package Peppy;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class PeppyLauncher {
+	
+	public static void main(String [] args) {
+		try {
+			String pathToJar = PeppyLauncher.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath();
+			U.p(pathToJar);
+			ProcessBuilder pb = new ProcessBuilder("java","-XX:+AggressiveHeap", "-classpath", pathToJar, "Peppy.Peppy");
+			pb.start();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
